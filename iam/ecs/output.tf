942b8858687046b58c06786c83c3c14f3a0ea1ed
Jerry Chang <therealjerrycan@gmail.com>
2022-01-16 20:12:15 -0800

    fix syntax issue 2

--- iam/ecs/output.tf
+++ iam/ecs/output.tf
@@ -1,5 +1,5 @@
 output "ecs_task_role_arn" {
-    value = aws_iam_role.ecs_task_role[0].arn
+  value = local.create_ecs_task_role ? aws_iam_role.ecs_task_role[0].arn : ""
 }
 
 output "ecs_execution_role_arn" {
