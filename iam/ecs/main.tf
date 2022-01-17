fababac354ccd126033a554e07b96247559a9bb0
Jerry Chang <therealjerrycan@gmail.com>
2022-01-16 20:04:17 -0800

    Only create role if custom iam is provided

--- iam/ecs/main.tf
+++ iam/ecs/main.tf
@@ -1,3 +1,13 @@
+locals {
+  # Only create if we have iam statements, otherwise don’t create a role
+  create_ecs_task_role = (
+    # specify to create task role
+    var.create_ecs_task_role
+    # specify custom iam statements
+    && var.ecs_task_iam_statements != null
+  )
+}
+
 data "aws_iam_policy_document" "ecs_execution" {
   version = "2012-10-17"
 
@@ -33,7 +43,7 @@ data "aws_iam_policy_document" "ecs_task" {
 
   ## Extend with iam permission statements
   dynamic "statement" {
-    for_each = var.ecs_task_iam_statements != null ? var.ecs_task_iam_statements : {}
+    for_each = local.create_ecs_task_role ? var.ecs_task_iam_statements : {}
     content {
       actions   = statement.value["actions"]
       effect    = statement.value["effect"]
@@ -43,14 +53,14 @@ data "aws_iam_policy_document" "ecs_task" {
 }
 
 resource "aws_iam_policy" "ecs_execution" {
-  count       = var.create_ecs_execution_role == true ? 1 : 0
+  count       = (var.create_ecs_execution_role == true) ? 1 : 0
   name        = "ecs-execution-role-policy"
   description = "Custom Policy for ECS Execution"
   policy      = data.aws_iam_policy_document.ecs_execution.json
 }
 
 resource "aws_iam_policy" "ecs_task" {
-  count       = var.create_ecs_task_role == true ? 1 : 0
+  count       = local.create_ecs_task_role ? 1 : 0
   name        = "ecs-task-role-policy"
   description = "Custom Policy for ECS Execution"
   policy      = data.aws_iam_policy_document.ecs_task.json
@@ -70,8 +80,9 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
   }
 }
 
+# Create roles
 resource "aws_iam_role" "ecs_task_role" {
-  count                = var.create_ecs_task_role == true ? 1 : 0
+  count                = local.create_ecs_task_role ? 1 : 0)
   name                 = "Policy-Ecs-Task-Role"
   assume_role_policy   = data.aws_iam_policy_document.ecs_task_assume_role.json
   description          = "IAM Role with permissions for ECS tasks"
@@ -84,16 +95,17 @@ resource "aws_iam_role" "ecs_execution_role" {
   description          = "IAM Role with permissions for ECS Execution of tasks"
 }
 
+# Attachments
+resource "aws_iam_policy_attachment" "task_role_attachment" {
+  count      = local.create_ecs_task_role ? 1 : 0
+  name       = "Policy-ECS-Task-Role-Policy"
+  policy_arn = aws_iam_policy.ecs_task[0].arn
+  roles      = [aws_iam_role.ecs_task_role[0].name]
+}
+
 resource "aws_iam_policy_attachment" "execution_role_attachment" {
   count      = var.create_ecs_execution_role == true ? 1 : 0
   name       = "Policy-ECS-Execution-Role-Policy"
   policy_arn = aws_iam_policy.ecs_execution[0].arn
   roles      = [aws_iam_role.ecs_execution_role[0].name]
-}
-
-resource "aws_iam_policy_attachment" "task_role_attachment" {
-  count      = var.create_ecs_task_role == true ? 1 : 0
-  name       = "Policy-ECS-Task-Role-Policy"
-  policy_arn = aws_iam_policy.ecs_task[0].arn
-  roles      = [aws_iam_role.ecs_task_role[0].name]
 }
